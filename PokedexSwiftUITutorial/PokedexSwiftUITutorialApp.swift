ea9928b1ff7fffac8b6b42fb763aadea3d5d2835
OlgaHook <olga_krjuckova@inbox.lv>
2021-08-26 09:09:02 +0300

    Initial Commit

---
+++ PokedexSwiftUITutorial/PokedexSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUITutorialApp.swift
+//  PokedexSwiftUITutorial
+//
+//  Created by olga.krjuckova on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
